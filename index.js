9324c6522b2346612963bcde6b02d70329966667
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-15 11:58:01 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // const BASE_URL = 'https://new-todo-json-server-vercel.vercel.app/todos/';
 const BASE_URL = 'http://localhost:8001/todos';
+
+const createForm = document.getElementById('create-form')//grab form for creating new todo
+const createTitleInput =  document.getElementById('create-title')//grab input field for todo title input
+const updateForm = document.getElementById('update-form')//grab form for updating todo
+const updateTitleInput = documetn.getElementById('update-title')//input title for updating todo
+const todoSection = document.getElementById('todos-section')//section for displaying all todos
+const singleTodoSection = document.getElementById('single-todo-section')//section for displaying single todo item
+
+
